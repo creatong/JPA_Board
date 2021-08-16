044e8de1cab0ff7b10ce35507fa55dda2211d3a8
creatong <jgh3482@gmail.com>
2021-08-16 18:57:05 +0900

    update pageNumber, searchText

--- src/main/java/com/jpatest/myhome/validator/BoardValidator.java
+++ src/main/java/com/jpatest/myhome/validator/BoardValidator.java
@@ -16,7 +16,7 @@ public class BoardValidator implements Validator {
     @Override
     public void validate(Object obj, Errors errors){
         Board bd = (Board) obj;
-        if(StringUtils.isEmpty(bd.getClass().getName())){
+        if(StringUtils.isEmpty(bd.getContent)){
             errors.rejectValue("content", "key", "내용을 입력하세요");
         }
     }
